49b02b8d9f54bd87fac741387304f96e77eab6da
Lyamin Artemiy <68056495+lyaminartemiy@users.noreply.github.com>
2023-05-29 12:08:02 +0300

    Update EfficientNetModel.py

--- EfficientNetModel.py
+++ EfficientNetModel.py
@@ -14,58 +14,78 @@ from time import sleep
 
 
 class EfficientNetModel:
+    """
+    Класс модели EfficientNet для классификации изображений.
+
+    Атрибуты:
+        version (str): Версия EfficientNet. В данном случае должно быть 'efficientnet-b1'.
+        path_to_model_dict (str): Путь к файлу со словарем весов модели. По умолчанию None.
+    
+    Методы:
+        None.
+
+    """
+
     def __init__(self, version, path_to_model_dict=None):
-        
+        """
+        Инициализация класса EfficientNetModel.
+
+        Аргументы:
+            version (str): Версия EfficientNet. В данном случае должно быть 'efficientnet-b1'.
+            path_to_model_dict (str): Путь к файлу со словарем весов модели. По умолчанию None.
+        """
+
         assert version == 'efficientnet-b1', "The version should be 'efficientnet-b1'"
-        
+
         # Создание модели версии - version
         self.model = EfficientNet.from_pretrained(version)
-        
+
         # заменяем последний слой
         self.model._fc = nn.Linear(1280, 3)
         self.model._fc.requires_grad_ = True
-        
+
         # История обучения и тестирования
         self.history = None
-        
+
         # Загрузка весов модели, если такая существует
-        if path_to_model_dict is None:
-            next
-        else:
+        if path_to_model_dict is not None:
             self.model.load_state_dict(torch.load(path_to_model_dict))
-            
+
         # установка доступного устройства
         self.DEVICE = torch.device("cpu")
         
     def load_model(self, path_to_model_dict):
         """
-        Метод для загрузки весов обученной модели
-        
+        Метод для загрузки весов обученной модели.
+
         Аргументы:
-        path_to_model_dict -- str путь до файла модели
+            path_to_model_dict (str): Путь до файла модели.
         """
         self.model.load_state_dict(torch.load(path_to_model_dict))
         
     def save_model(self, name_model_dict):
         """
-        Метод для сохранения весов обученной модели
-        
+        Метод для сохранения весов обученной модели.
+
         Аргументы:
-        name_model_dict -- str название файла для сохранения
+            name_model_dict (str): Название файла для сохранения.
         """
         torch.save(self.model.state_dict(), name_model_dict)
+
         
     def train(self, max_epochs, optimizer, loss_fn, train_dataloader, val_dataloader):
         """
-        Метод для обучения нейросети
+        Метод для обучения нейросети.
 
         Аргументы:
-        max_epochs -- количество эпох int
-        optimizer -- torch.optim оптимизатор
-        loss_fn -- функция потерь
+            max_epochs (int): Количество эпох.
+            optimizer (torch.optim.Optimizer): Оптимизатор.
+            loss_fn: Функция потерь.
+            train_dataloader (torch.utils.data.DataLoader): Загрузчик данных для обучения.
+            val_dataloader (torch.utils.data.DataLoader): Загрузчик данных для валидации.
 
         Возвращает:
-        словарь со значениями функции потерь в процессе обучения
+            dict: Словарь со значениями функции потерь в процессе обучения.
         """
 
         if not torch.cuda.is_available():
@@ -79,9 +99,7 @@ class EfficientNetModel:
         }
 
         for epoch in trange(max_epochs, desc="Progress Bar", unit="carrots"):
-
             train_losses_per_epoch = []
-
             running_corrects = 0
             processed_data = 0
 
@@ -89,7 +107,6 @@ class EfficientNetModel:
             self.model.train()
 
             for X_batch, y_batch in train_dataloader:
-
                 X_batch, y_batch = X_batch.to(self.DEVICE), y_batch.to(self.DEVICE).type(torch.LongTensor)
                 y_batch = y_batch.view(1, -1)[0]
 
@@ -109,7 +126,6 @@ class EfficientNetModel:
                 optimizer.step()
 
                 train_losses_per_epoch.append(loss.item())
-                
                 preds = torch.argmax(output, 1)
                 running_corrects += torch.sum(preds == y_batch.data)
                 processed_data += X_batch.size(0)
@@ -121,17 +137,14 @@ class EfficientNetModel:
             self.model.eval()
 
             valid_losses_per_epoch = []
-
             running_corrects = 0
             processed_data = 0
 
             for X_batch, y_batch in val_dataloader:
-
                 X_batch, y_batch = X_batch.to(self.DEVICE), y_batch.to(self.DEVICE).type(torch.LongTensor)
                 y_batch = y_batch.view(1, -1)[0]
 
                 with torch.no_grad():
-
                     # Выход модели
                     output = self.model(X_batch.float())
 
@@ -139,7 +152,6 @@ class EfficientNetModel:
                     loss = loss_fn(output, y_batch)
 
                 valid_losses_per_epoch.append(loss.item())
-                
                 preds = torch.argmax(output, 1)
                 running_corrects += torch.sum(preds == y_batch.data)
                 processed_data += X_batch.size(0)
@@ -151,30 +163,31 @@ class EfficientNetModel:
     
     def freeze_weights(self, number_freezing):
         """
-        Метод для заморозки весов слоёв нейросети
-        
+        Метод для заморозки весов слоев нейросети.
+
         Аргументы:
-        number_freezing -- число замороженных слоёв
+            number_freezing (int): Число замороженных слоев.
         """
         for i, param in enumerate(self.model.parameters()):
             param.requires_grad = bool(i > number_freezing)
             
     def predict(self, data_loader):
         """
-        Метод для предсказывания классов на тестовой выборке
-        
+        Метод для предсказания классов на тестовой выборке.
+
         Аргументы:
-        data -- torch.utils.data.dataloader даталоадер для теста
+            data_loader (torch.utils.data.DataLoader): Даталоадер для тестирования.
+
+        Возвращает:
+            numpy.ndarray: Массив вероятностей предсказанных классов.
         """
         # Режим тестирования
         self.model.eval()
         
         with torch.no_grad():
-            
             logits = []
             
             for X_batch in data_loader:
-
                     X_batch = X_batch.to(self.DEVICE)
 
                     # Выход модели
@@ -187,16 +200,18 @@ class EfficientNetModel:
     
     def predict_item(self, mini_batch):
         """
-        Метод для предсказывания класса изображения
-        
+        Метод для предсказания класса изображения.
+
         Аргументы:
-        data -- torch.utils.data.dataloader даталоадер для теста
+            mini_batch (torch.Tensor): Мини-пакет изображений для предсказания класса.
+
+        Возвращает:
+            numpy.ndarray: Массив вероятностей предсказанного класса.
         """
         # Режим тестирования
         self.model.eval()
         
         with torch.no_grad():
-            
             # Выход модели
             output = self.model(mini_batch.float()).cpu()
         
